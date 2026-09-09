ceaa1375096009cbfd102209665a4c7627c29d01
bvanchri <vanchristamorjunior.balouckou@ynov.com>
2026-09-09 14:34:16 +0200

    {FIX / ADD} fonction pour les lignes

--- wordle/src/components/Board.tsx
+++ wordle/src/components/Board.tsx
@@ -7,3 +7,45 @@ interface BoardProps {
   statuses: LetterStatus[][];
 }
 
+function Board({
+  guesses,
+  currentWord,
+  statuses,
+}: BoardProps) {
+  const rows = Array.from({
+    length: 6,
+  });
+
+  return (
+    <div className="board">
+      {rows.map((_, rowIndex) => {
+        let word = "";
+
+        let rowStatuses:
+          | LetterStatus[]
+          | undefined;
+
+        if (rowIndex < guesses.length) {
+          word = guesses[rowIndex];
+
+          rowStatuses =
+            statuses[rowIndex];
+        }
+
+        if (rowIndex === guesses.length) {
+          word = currentWord;
+        }
+
+        return (
+          <Row
+            key={rowIndex}
+            word={word}
+            statuses={rowStatuses}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Board;
