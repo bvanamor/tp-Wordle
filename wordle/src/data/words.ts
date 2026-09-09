434b382f3bb2f731f504b8b20d68602f120e4ade
bvanchri <vanchristamorjunior.balouckou@ynov.com>
2026-09-09 16:08:15 +0200

    {ADD} ajout mots du jour

---
+++ wordle/src/data/words.ts
@@ -0,0 +1,18 @@
+export const TARGET_WORD = "ARBRE";
+
+export const WORD_LENGTH = 5;
+
+export const MAX_ATTEMPTS = 6;
+
+export const WORD_LIST = [
+  "ARBRE",
+  "CHIEN",
+  "TABLE",
+  "LIVRE",
+  "FLEUR",
+  "ROUGE",
+  "NOIRE",
+  "BLANC",
+  "PORTE",
+  "MAISON",
+];
